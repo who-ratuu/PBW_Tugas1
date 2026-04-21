ab7ccc35ac9fdddac129fd18ed575bf70b438a9c
RATU BILQIS FEBRIYANI <2410631170044@student.unsika.ac.id>
2026-04-21 11:46:38 +0700

    Delete Tugas8_PBW directory

--- Tugas8_PBW/menu.php
+++
@@ -1,7 +0,0 @@
-    <h2>Menu Soal</h2>
-    <ul>
-        <li><a href="soal1.php">Soal 1 - Switch Kendaraan</a></li>
-        <li><a href="soal2.php">Soal 2 - Bilangan Genap</a></li>
-        <li><a href="soal3.php">Soal 3 - Foreach Hewan</a></li>
-        <li><a href="soal4.php">Soal 4 - Ternary Genap/Ganjil</a></li>
-    </ul>
